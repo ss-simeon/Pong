55ae15e02a43495772d4cdd0a7154599a5830a8d
ss-simeon <simeonflorea@icloud.com>
2024-05-13 21:38:10 -0700

    [] Set up FPS counter and its toggle. Set up Media folder for fonts and others in the future

--- src/Game.hpp
+++ src/Game.hpp
@@ -21,6 +21,7 @@ private:
 	void render();
 
 	sf::RenderWindow m_window;
+    size_t m_game_fps;
 
     PongUtils m_pong_utils;
     PongGraphics m_pong_graphics;
@@ -30,20 +31,22 @@ private:
 };
 
 Game::Game()
-	: m_pong_graphics(1920u, 1080u), m_pong_utils(), 
+    : m_pong_graphics(1920u, 1080u), m_pong_utils(),
     m_window(sf::VideoMode(1920u, 1080u), m_pong_utils.construct_window_title()),
-    m_player_one(true, 50.f, 250.f), m_player_two(false, 50.f, 250.f), m_ball()
+    m_player_one(true, 50.f, 250.f), m_player_two(false, 50.f, 250.f), m_ball(), m_game_fps{ 0u }
 {
-	m_window.setVerticalSyncEnabled(true);
+    m_window.setVerticalSyncEnabled(true);
 }
 
 void Game::run()
 {
     while (m_window.isOpen())
-    {
+    {        
         process_events();
         update();
         render();
+
+        m_game_fps = m_pong_utils.update_fps().value_or(m_game_fps);     
     }
 }
 
@@ -56,14 +59,23 @@ void Game::process_events()
         {
             m_window.close();
         }
+
+        if (event.type == sf::Event::KeyPressed)
+        {
+            if (event.key.scancode == sf::Keyboard::Scan::F1)
+            {
+                m_pong_graphics.handle_show_hide_fps_counter();
+            }
+        }
     }
 
     m_player_one.handle_move();
-    m_ball.move();    
+    m_ball.move();
 }
 
 void Game::update()
 {
+    m_pong_graphics.set_text_string(std::to_string(m_game_fps));
     m_ball.notify_of_player_positions(m_player_one.get_rect(), m_player_two.get_rect());
     m_ball.check_round_win();
 }
@@ -80,6 +92,11 @@ void Game::render()
     {
         m_window.draw(dividing_rect);
     }
+
+    if (m_pong_graphics.get_fps_show())
+    {
+        m_window.draw(m_pong_graphics.get_text());
+    }    
 
     m_window.display();
 }
--- src/PongUtils.hpp
+++ src/PongUtils.hpp
@@ -1,14 +1,23 @@
 #pragma once
 
+#include <queue>
+#include <optional>
+
+#include <SFML/System/Clock.hpp>
+
 #include "Config.h"
 
 class PongUtils
 {
 public:
-	PongUtils() {}
+    PongUtils() : m_clock(), m_fps_queue() {}
 
     sf::String construct_window_title();
+    std::optional<size_t> update_fps();
 private:
+    // FPS Counter
+    sf::Clock m_clock;
+    std::queue<size_t> m_fps_queue;
 };
 
 sf::String PongUtils::construct_window_title()
@@ -18,4 +27,27 @@ sf::String PongUtils::construct_window_title()
     window_bar_title += ".";
     window_bar_title += std::to_string(PingPong_VERSION_MINOR);
     return window_bar_title;
+}
+
+std::optional<size_t> PongUtils::update_fps()
+{
+    size_t avg_fps = 0u;
+
+    float current_time = m_clock.restart().asSeconds();
+    float fps = 1u / (current_time);
+    m_fps_queue.push(fps);
+
+    if (m_fps_queue.size() == 20)
+    {
+        float total_fps = 0.f;
+        for (; !m_fps_queue.empty(); m_fps_queue.pop())
+        {
+            total_fps += m_fps_queue.front();
+        }
+
+        avg_fps = total_fps / 20;
+        return avg_fps;
+    }
+
+    return std::nullopt;
 }

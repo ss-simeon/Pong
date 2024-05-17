da8d65df1b4420c434b31e1601019d4d58512d42
ss-simeon <simeonflorea@icloud.com>
2024-05-16 22:04:32 -0700

    [] Enemy paddle reacts to ball

--- src/Game.hpp
+++ src/Game.hpp
@@ -3,12 +3,14 @@
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 
-#include "ResourceHolder.hpp"
+#include <memory>
 
+#include "Ball.hpp"
+#include "Player.hpp"
+#include "Enemy.hpp"
+#include "ResourceHolder.hpp"
 #include "PongGraphics.hpp"
 #include "PongUtils.hpp"
-#include "Player.hpp"
-#include "Ball.hpp"
 
 class Game
 {
@@ -29,20 +31,23 @@ private:
 
     PongUtils m_pong_utils;
     PongGraphics m_pong_graphics;
-	Player m_player_one;
-	Player m_player_two;
-	Ball m_ball;
+    Ball m_ball;
+
+	std::unique_ptr<Player> m_player_one;
+    std::unique_ptr<Enemy> m_player_two;
 };
 
 Game::Game()
-    : m_pong_graphics(1920u, 1080u), m_pong_utils(),
-    m_window(sf::VideoMode(1920u, 1080u), m_pong_utils.construct_window_title()),
-    m_player_one(true, 50.f, 250.f), m_player_two(false, 50.f, 250.f), m_ball(), m_game_fps{ 0u }, m_font_holder()
+    : m_pong_graphics(1920u, 1080u), m_pong_utils(), m_player_one(), m_player_two(),
+    m_window(sf::VideoMode(1920u, 1080u), m_pong_utils.construct_window_title()), m_ball(), m_game_fps{ 0u }, m_font_holder()
 {
     m_window.setVerticalSyncEnabled(true);
 
     m_font_holder.load(Fonts::fps_font, "../Media/Fonts/Basic-Regular.ttf");
     m_pong_graphics.set_font(m_font_holder.get(Fonts::fps_font));
+
+    m_player_one = std::unique_ptr<Player>(new Player(true, 50.f, 250.f));
+    m_player_two = std::unique_ptr<Enemy>(new Enemy(false, 50.f, 250.f));
 }
 
 void Game::run()
@@ -53,7 +58,7 @@ void Game::run()
         update();
         render();
 
-        m_game_fps = m_pong_utils.update_fps().value_or(m_game_fps);     
+        m_game_fps = m_pong_utils.update_fps().value_or(m_game_fps);
     }
 }
 
@@ -76,14 +81,16 @@ void Game::process_events()
         }
     }
 
-    m_player_one.handle_move();
+    m_player_one->handle_move();
+    m_player_two->handle_move();
     m_ball.move();
 }
 
 void Game::update()
 {
+    m_player_two->notify_of_ball_position(m_ball.get_rect());
     m_pong_graphics.set_text_string(std::to_string(m_game_fps));
-    m_ball.notify_of_player_positions(m_player_one.get_rect(), m_player_two.get_rect());
+    m_ball.notify_of_player_positions(m_player_one->get_rect(), m_player_two->get_rect());
     m_ball.check_round_win();
 }
 
@@ -91,8 +98,8 @@ void Game::render()
 {
     m_window.clear();
 
-    m_window.draw(m_player_one.get_rect());
-    m_window.draw(m_player_two.get_rect());
+    m_window.draw(m_player_one->get_rect());
+    m_window.draw(m_player_two->get_rect());
     m_window.draw(m_ball.get_rect());
 
     for (auto& dividing_rect : m_pong_graphics.get_dividing_lane())
